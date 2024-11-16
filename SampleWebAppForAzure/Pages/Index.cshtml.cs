da0b1ee25192281c4da3eba6b9371b67013889c0
Sandeep Pachal <pachal.sandeep@gmail.com>
2024-11-16 19:19:32 +0530

    Added Greeting Configuration

--- SampleWebAppForAzure/Pages/Index.cshtml.cs
+++ SampleWebAppForAzure/Pages/Index.cshtml.cs
@@ -5,16 +5,18 @@ namespace SampleWebAppForAzure.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
+            _configuration = configuration;
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
